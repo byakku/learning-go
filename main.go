808f6c9b04eb70724edf9b5ad519484c309b243e
byakku <dante@byakku.net>
2019-10-12 15:29:19 +0200

    gofmt

--- main.go
+++ main.go
@@ -1,52 +1,52 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"flag"
-	"strings"
-	"strconv"
 	"encoding/csv"
 	"encoding/json"
+	"flag"
+	"fmt"
 	"io"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type User struct {
-  Id    int    `json:"id"`
-  Name  string `json:"name"`
-  Home  string `json:"home"`
-  Shell string `json:"shell"`
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Home  string `json:"home"`
+	Shell string `json:"shell"`
 }
 
 func main() {
-  path, format := parseFlags()
-  users := collectUsers()
+	path, format := parseFlags()
+	users := collectUsers()
 
-  var output io.Writer
+	var output io.Writer
 
-  if path != "" {
-	  f, err := os.Create(path)
-	  handleError(err)
-	  defer f.Close()
-	  output = f
-  } else {
-	  output = os.Stdout
-  }
-
-  if format == "json" {
-	data, err := json.MarshalIndent(users, "", "  ")
-	handleError(err)
-	output.Write(data)
-  } else if format == "csv" {
-	output.Write([]byte("name,id,home,shell\n"))
-	writer := csv.NewWriter(output)
-
-	for _, user := range users {
-		err := writer.Write([]string{user.Name, strconv.Itoa(user.Id), user.Home, user.Shell})
+	if path != "" {
+		f, err := os.Create(path)
 		handleError(err)
+		defer f.Close()
+		output = f
+	} else {
+		output = os.Stdout
 	}
-	writer.Flush()
-  }
+
+	if format == "json" {
+		data, err := json.MarshalIndent(users, "", "  ")
+		handleError(err)
+		output.Write(data)
+	} else if format == "csv" {
+		output.Write([]byte("name,id,home,shell\n"))
+		writer := csv.NewWriter(output)
+
+		for _, user := range users {
+			err := writer.Write([]string{user.Name, strconv.Itoa(user.ID), user.Home, user.Shell})
+			handleError(err)
+		}
+		writer.Flush()
+	}
 }
 
 func parseFlags() (path, format string) {
@@ -65,45 +65,42 @@ func parseFlags() (path, format string) {
 }
 
 func handleError(err error) {
-  if err != nil {
-    fmt.Println("Error:", err)
-    os.Exit(1)
-  }
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 }
 
 func collectUsers() (users []User) {
-  f, err := os.Open("/etc/passwd")
-  handleError(err)
-  defer f.Close()
-  
-  reader := csv.NewReader(f)
-  reader.Comma = ':'
+	f, err := os.Open("/etc/passwd")
+	handleError(err)
+	defer f.Close()
 
-  lines, err := reader.ReadAll()
+	reader := csv.NewReader(f)
+	reader.Comma = ':'
 
-  handleError(err)
+	lines, err := reader.ReadAll()
 
+	handleError(err)
 
-  for _, line := range lines {
-  
-    id, err := strconv.ParseInt(line[2], 10, 64)
-    handleError(err)
-  
+	for _, line := range lines {
 
-  if id < 1000 {
-    continue
-  }
+		id, err := strconv.ParseInt(line[2], 10, 64)
+		handleError(err)
 
-  
-  user := User{
-    Name: line[0],
-    Id: int(id), 
-    Home: line[5],
-    Shell: line[6],
-  }
-  
-  users = append(users, user)
-}
-  
-  return
+		if id < 1000 {
+			continue
+		}
+
+		user := User{
+			Name:  line[0],
+			ID:    int(id),
+			Home:  line[5],
+			Shell: line[6],
+		}
+
+		users = append(users, user)
+	}
+
+	return
 }
